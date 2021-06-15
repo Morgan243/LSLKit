76ca5549684b34218416ae162f34f2fdd55d829d
Morgan Stuart <morgansstuart243@gmail.com>
2021-06-14 22:14:01 -0400

    remove old code and comments

--- setup.py
+++ setup.py
@@ -1,11 +1,8 @@
 from setuptools import setup, find_packages
 setup(name='lslkit',
       version='0.1',
-      description='API For LSL Development and Experiments',
+      description='API for PyLSL Development and Experiments',
       author='Morgan Stuart',
-      #packages=['mmz'],
       packages=find_packages(),
-      #modules=['feature_processing', 'torch_models'],
       requires=['pylsl', 'numpy', 'pandas',
-                #'sklearn', 'torch', 'torchvision',
                 'attrs'])
